7478fb13a80b85f6960b26dd98eae7c448d671ac
Fran Delgado <thinway@gmail.com>
2017-01-27 10:56:48 +0100

    Creados los métodos de operaciones con Fracciones.

--- src/org/thinway/Fraccion.java
+++ src/org/thinway/Fraccion.java
@@ -32,9 +32,18 @@ public class Fraccion {
      * @param denominador entero que especifica el denominador.
      */
     public Fraccion(int numerador, int denominador){
-        this.setNumerador(numerador);
-        this.setDenominador(denominador);
+        if(denominador != 0){
+            this.setNumerador(numerador);
+            this.setDenominador(denominador);
+        } else {
+            this.setNumerador(numerador);
+            this.setDenominador(1);
+        }
+
+
+        this.simplificar();
     }
+
     // Accessors
 
     /**
@@ -81,5 +90,105 @@ public class Fraccion {
      */
     @Override public String toString(){
         return this.getNumerador() + "/" + denominador;
+    }
+
+    // Operaciones
+
+    public void sumar(Fraccion frac){
+        int mcm = minimoComunMultiplo(this.getDenominador(), frac.getDenominador());
+
+        this.setNumerador(  mcm/this.getDenominador() * this.getNumerador() +
+                            mcm/frac.getDenominador() * frac.getNumerador() );
+        this.setDenominador( mcm );
+
+        this.simplificar();
+
+    }
+
+
+    /**
+     * Multiplica dos fracciones y guarda el resultado en la instancia
+     * que lo invoca.
+     *
+     * @param frac Fracción que será el segundo operando de la multiplicación.
+     */
+    public void multiplicar(Fraccion frac){
+        this.setNumerador(this.getNumerador()*frac.getNumerador());
+        this.setDenominador(this.getDenominador()*frac.getDenominador());
+
+        this.simplificar();
+    }
+
+    /**
+     * Multiplica dos fracciones y guarda el resultado en la instancia
+     * que lo invoca.
+     *
+     * @param frac Fracción que será el segundo operando de la multiplicación.
+     */
+    public Fraccion multiply(Fraccion frac){
+        Fraccion resultado = new Fraccion();
+
+        resultado.setNumerador(this.getNumerador()*frac.getNumerador());
+        resultado.setDenominador(this.getDenominador()*frac.getDenominador());
+
+        resultado.simplificar();
+
+        return resultado;
+    }
+
+    /**
+     * Divide dos fracciones y guarda el resultado en la instancia
+     * que lo invoca.
+     *
+     * @param frac Fracción que será el segundo operando de la multiplicación.
+     */
+    public void dividir(Fraccion frac){
+        this.setNumerador(this.getNumerador()*frac.getDenominador());
+        this.setDenominador(this.getDenominador()*frac.getNumerador());
+
+        this.simplificar();
+    }
+
+
+    // Helpers
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public int mcd(int a, int b){
+        // a: 537, b: 50
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a < b){
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+
+        int resto = a % b;
+
+        while( resto!= 0){
+            b = resto;
+            resto = a % b;
+            a = b;
+        }
+
+        return b;
+    }
+
+    private int minimoComunMultiplo(int a, int b){
+        return ((a*b) / mcd(a,b));
+    }
+
+    private void simplificar(){
+        int maxDiv = mcd(this.getNumerador(),
+                        this.getDenominador());
+
+        this.setNumerador(this.getNumerador()/maxDiv);
+        this.setDenominador(this.getDenominador()/maxDiv);
     }
 }
--- src/org/thinway/Main.java
+++ src/org/thinway/Main.java
@@ -3,8 +3,14 @@ package org.thinway;
 public class Main {
 
     public static void main(String[] args) {
-        Fraccion a = new Fraccion(5);
+        Fraccion fraccion1 = new Fraccion(2,0);
+        Fraccion fraccion2 = new Fraccion(2,2);
 
-        System.out.println(a);
+        System.out.print(fraccion1 + " + " + fraccion2 + " = " );
+
+        fraccion1.sumar(fraccion2);
+
+        System.out.println(fraccion1);
+
     }
 }
